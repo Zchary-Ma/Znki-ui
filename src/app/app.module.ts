500f4efa08bdb03985a9607ed2420c0cfcbb8b3d
zach-ma <alexma4cv@gmail.com>
2020-11-15 00:01:03 +0800

    add: ui module | storybook | init ui structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,45 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbButtonModule,
+  NbSidebarModule,
+  NbIconModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { StatusComponent } from './shared/components/status/status.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+
+const NbModules = [
+  NbSidebarModule.forRoot(), // NbSidebarModule.forRoot(), //if this is your app.module
+  NbThemeModule.forRoot({ name: 'default' }),
+  NbButtonModule,
+  NbLayoutModule,
+  NbIconModule,
+  NbEvaIconsModule,
+  NbSidebarModule,
+];
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    FooterComponent,
+    HeaderComponent,
+    StatusComponent,
+    SidebarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...NbModules,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/components/status/status.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status',
+  styleUrls: ['./status.component.scss'],
+  template: ``,
+})
+export class StatusComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
