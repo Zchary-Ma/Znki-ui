d6d759497f0d7cf2fc8fafe51098b24a8863cc09
zach-ma <alexma4cv@gmail.com>
2020-11-17 23:13:41 +0800

    well, nebular sucks.

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,15 +1,92 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LocalDataSource } from 'ng2-smart-table';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  settings = {
+    // add: {
+    //   addButtonContent: '<i class="nb-plus"></i>',
+    //   createButtonContent: '<i class="nb-checkmark"></i>',
+    //   cancelButtonContent: '<i class="nb-close"></i>',
+    // },
+    // edit: {
+    //   editButtonContent: '<i class="nb-edit"></i>',
+    //   saveButtonContent: '<i class="nb-checkmark"></i>',
+    //   cancelButtonContent: '<i class="nb-close"></i>',
+    // },
+    // delete: {
+    //   deleteButtonContent: '<i class="nb-trash"></i>',
+    //   confirmDelete: true,
+    // },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      firstName: {
+        title: 'First Name',
+        type: 'string',
+      },
+      lastName: {
+        title: 'Last Name',
+        type: 'string',
+      },
+      username: {
+        title: 'Username',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      age: {
+        title: 'Age',
+        type: 'number',
+      },
+    },
+  };
 
-  constructor() { }
+  source: LocalDataSource = new LocalDataSource();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit() {}
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
   }
 
+  onSearch(query: string = '') {
+    this.source.setFilter(
+      [
+        // fields we want to include in the search
+        {
+          field: 'id',
+          search: query,
+        },
+        {
+          field: 'name',
+          search: query,
+        },
+        {
+          field: 'username',
+          search: query,
+        },
+        {
+          field: 'email',
+          search: query,
+        },
+      ],
+      false
+    );
+    // second parameter specifying whether to perform 'AND' or 'OR' search
+    // (meaning all columns should contain search query or at least one)
+    // 'AND' by default, so changing to 'OR' by setting false here
+  }
 }
--- src/app/pages/list/list.module.ts
+++ src/app/pages/list/list.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ListRoutingModule } from './list-routing.module';
 import { ListComponent } from './list.component';
-
+import { NbCardModule } from '@nebular/theme';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [
-    CommonModule,
-    ListRoutingModule
-  ]
+  imports: [CommonModule, ListRoutingModule, NbCardModule, Ng2SmartTableModule],
+  providers: [],
 })
-export class ListModule { }
+export class ListModule {}
