760106632d0b280af70bd8ad492752552dbfe069
zach-ma <alexma4cv@gmail.com>
2020-11-20 11:42:13 +0800

    add: table module

--- src/app/pages/list/list.module.ts
+++ src/app/pages/list/list.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListRoutingModule } from './list-routing.module';
 import { ListComponent } from './list.component';
+import { NzTableModule } from 'ng-zorro-antd/table';
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [CommonModule, ListRoutingModule],
+  imports: [CommonModule, ListRoutingModule, NzTableModule],
   providers: [],
 })
 export class ListModule {}
